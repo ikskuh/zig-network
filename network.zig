9c83fcb28911efb3fc5e511cb5eb3df82d418e1a
g41797 <g41797@gmail.com>
2024-10-06 09:36:43 +0300

    Try to fix  "Darwin: connection-mode socket was connected already"

--- network.zig
+++ network.zig
@@ -752,7 +752,7 @@ pub const Socket = struct {
         }
         while (true) {
             const rc = std.posix.system.sendto(sockfd, buf.ptr, buf.len, flags, dest_addr, addrlen);
-            switch (std.posix.system.errno(rc)) {
+            switch (std.posix.errno(rc)) {
                 .SUCCESS => return @intCast(rc),
 
                 .ACCES => return error.AccessDenied,
